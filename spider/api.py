e5c335c2b9f8366c0ef0d6c3cf022760a485d4b9
jiangwei <jiangwei@mafengwo.com>
2019-01-18 14:52:56 +0800

    fix

--- spider/api.py
+++ spider/api.py
@@ -7,11 +7,17 @@ import requests
 
 def get(url):
     sleep(random.randint(0,5))
-    return requests.get(url)
+    try:
+        return requests.get(url,timeout=10)
+    except:
+        return requests.get(url, timeout=10)
 
 
 def getList(cityid, kw, start, length):
     url = 'https://fe-api.zhaopin.com/c/i/sou?start=' + str(start) + 'pageSize=' + str(length) + '&cityId=' + str(
         cityid) + '&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&sortType=publish&kw=' + str(
         kw) + '&kt=3&_v=0.17996222&x-zp-page-request-id=e8d2c03d3c4347a9b5edffa03367d90d-1547646999572-254944'
-    return json.loads(get(url).text)
+    try:
+        return json.loads(get(url).text)
+    except:
+        return {'code':0}
